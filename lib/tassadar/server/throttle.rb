c772a792ccd704ac22df887da5ea8eb5b4dc4ab3
Logan Koester <logan@logankoester.com>
2013-01-03 03:36:22 -0500

    Patch for upstream throttle bug

--- lib/tassadar/server/throttle.rb
+++ lib/tassadar/server/throttle.rb
@@ -3,19 +3,26 @@ require 'rack/throttle'
 module Tassadar
   module Server
     class Throttle < Rack::Throttle::Hourly
+
       def client_identifier(request)
-        
         # Determine whether the user should be identified by a unique
         # header (for example, a username or token), or simply by IP
         # for the purpose of rate limiting.
-        
         if ENV['USER_HEADER'] && request.env.has_key?(ENV['USER_HEADER'])
           request.env[ENV['USER_HEADER']].to_s
         else
           request.ip.to_s
         end
-
       end
+
+      # Patch for unmerged https://github.com/datagraph/rack-throttle/pull/6
+      def http_error(code, message = nil, headers = {})
+        [ code,
+          { 'Content-Type' => 'text/plain; charset=utf-8' }.merge(headers),
+          Array( http_status(code) + (message.nil? ? "\n" : " (#{message})\n") )
+        ]
+      end
+      
     end
   end
 end
